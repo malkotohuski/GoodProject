bd7659431b9a8b5111ccf378bd4c5afbce099a12
Daniel Dimitrov <malkotohuski@gmail.com>
2023-11-02 13:09:08 +0200

    home page

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hello = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default hello
+
+const styles = StyleSheet.create({})
